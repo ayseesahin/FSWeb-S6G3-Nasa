03dda249b2fb2bbaf20310a4ab28f41f07524180
Ayşe Nur Şahin <aysesah-95@hotmail.com>
2023-03-29 19:55:33 +0300

    not finished yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import OneComponent from "./components/OneComponent";
 
 function App() {
+  const [nasaData, setNasaData] = useState();
+  // const [dataPicker, setDataPicker] = useState(new Date("2022-12-31"));
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2010-06-04",
+        {
+          params: {
+            api_key: "wW4klK2HSiFYDP3utuSnAGbcWTdzaMau7pbCd9pG",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        setNasaData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div>
+        {nasaData ? (
+          <div>
+            <img src={nasaData.url} alt="NASA" />
+            <h1>{nasaData.title}</h1>
+            <p>{nasaData.explanation}</p>
+          </div>
+        ) : (
+          <p>Yükleniyor...</p>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/OneComponent.js
@@ -0,0 +1,20 @@
+// import React from "react";
+
+// export default function OneComponent(props) {
+//   const { data, dataChange, currentDate } = props;
+//   if (!data) return <h3>Yükleniyor...</h3>;
+//   return (
+//     <div>
+//       <p>{data.date}</p>
+//       <label htmlFor=""></label>
+//       <input type={}></input>
+//       <p>{data.explanation}</p>
+//       <p>{data.hdurl}</p>
+//       <p>{data.media_type}</p>
+//       <img src={data.hdurl} alt={data.title} width="500" height="500" />
+//       <p>{data.service_version}</p>
+//       <p>{data.title}</p>
+//       <p>{data.url}</p>
+//     </div>
+//   );
+// }
